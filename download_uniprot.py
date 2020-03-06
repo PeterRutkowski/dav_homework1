acd00f4c44a70031da05ca5ad622d89ff23ec1e6
PeterR <piotrrutkowski97@gmail.com>
2020-03-06 13:32:56 +0100

    Update download_uniprot.py

--- download_uniprot.py
+++ download_uniprot.py
@@ -3,13 +3,13 @@
 import os
 import random
 
-
-folder_name = 'Bacteria' # possible folder names: Bacteria, Viruses, Archaea, Eukaryota
+folder_name = 'Archaea' # possible folder names: Archaea, Bacteria, Eukaryota, Viruses
 # download each folder one by one and save files in data/uniprot_<foldername in lowercase>/
-# for example: data/uniprot_bacteria/
+# for example: data/uniprot_archaea/
 
 target_url = 'ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/'+ folder_name +'/'
 
+# calculate how many supported fasta files there are
 import urllib.request
 counter = 0
 for line in urllib.request.urlopen(target_url):
@@ -17,10 +17,12 @@ for line in urllib.request.urlopen(target_url):
         if line.decode('utf-8')[-10] == 'f':
             counter = counter + 1
 
+# choose random 200 indices
 l = random.sample(range(counter), 200)
 print(len(l))
 l.sort()
 
+# download 200 files with indices generated above
 counter = 0
 i = 0
 for line in urllib.request.urlopen(target_url):
